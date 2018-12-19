18751883267665f11c6c1277d95439e3bd6c57df
sulgod <sulemanovaaa@icloud.com>
2018-12-20 01:59:17 +0300

    added swap

--- src/User.hs
+++ src/User.hs
@@ -12,7 +12,7 @@ import Data.Char
 gener = mkStdGen 42
 rrr = evalRand (generateField 5 5) gener
 
-control rrr = replicateM_ 2 $ msg1 >> getLine >>= toInt 
+control gm = replicateM_ 2 $ msg1 >> getLine >>= toInt >> return gm
 	where 
 		msg1 = putStrLn "write coordinates (ex: 1 2 2 2)"
 		toInt s = do
@@ -21,7 +21,7 @@ control rrr = replicateM_ 2 $ msg1 >> getLine >>= toInt
 			let x1 = digitToInt (s !! 4) 
 			let y1 = digitToInt (s !! 6) 
 			let x = digitToInt (s !! 0) 
-			print $ replaceInToRandom (x, z) rrr
+			print $ swapObj (x, z) (x1, y1) gm
 --		doubleMe :: Integer -> Integer
 --		doubleMe x = x + x  
 -- 
