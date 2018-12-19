02c29597c2860f4952e8b871191049821df85d90
sulgod <sulemanovaaa@icloud.com>
2018-12-19 20:14:47 +0300

    updated dialog

--- src/User.hs
+++ src/User.hs
@@ -1,11 +1,7 @@
 module User where
 
+import Field
 import Control.Monad
 
-control 0 = return ()
-control n = do
-  putStrLn "write coordinates and direction (ex: 1 2 l)"
-  getLine
-  control (n-1)
-
-main = control 10
+control = replicateM_ 10 $ msg1 >> getLine >>=  putStrLn
+	where msg1 = putStrLn "write coordinates and direction (ex: 1 2 l)"
